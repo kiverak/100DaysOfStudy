d5bdf5fc453076328cc0ed2a328d170be4abbb9b
vitalii.b <vitalii.b@halykmarket.com>
2024-11-20 18:02:03 +0500

    create config service

--- eureka-client/src/main/java/uz/kiverak/micro/demo/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/uz/kiverak/micro/demo/eurekaclient/controller/TestController.java
@@ -1,13 +1,17 @@
 package uz.kiverak.micro.demo.eurekaclient.controller;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 @RequestMapping("/main")
 public class TestController {
 
+    @Value("${eureka.instance.instance-id}")
+    private String id;
+
     @GetMapping("/test")
     public String test() {
-        return "test";
+        return id;
     }
 }
